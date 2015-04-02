eb1f70c83bd5ccf25bb2c91204cd9d5a6da89be8
Andy <thatandyrose@gmail.com>
2015-04-02 12:26:46 +0100

    changed timezone

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module Budge
     config.autoload_paths += %W(#{config.root}/app/services)
     config.autoload_paths += %W(#{config.root}/lib/services)
 
+    config.time_zone = 'Eastern Time (US & Canada)'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
