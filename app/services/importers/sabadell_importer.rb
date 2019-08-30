bcea9f0080262293c8baf2b3049b91008c742d0b
thatandyrose <thatandyrose@gmail.com>
2019-08-30 17:39:52 +0200

    added sabadell importer

--- app/services/importers/sabadell_importer.rb
+++ app/services/importers/sabadell_importer.rb
@@ -4,20 +4,19 @@ module Importers
 
     def import
       Transaction.transaction do
-
         CsvParser.new(@uri, col_sep: ';').iterate_csv do |row|
-          is_income = row[:amount].prevsent? && row[:paid_in_gbp].to_d > 0
+          is_income = row[:amount].to_d > 0
 
-          description = row[:description].to_s.encode('UTF-8', invalid: :replace, undef: :replace)
+          description = row[:item].to_s.encode('UTF-8', invalid: :replace, undef: :replace)
 
           t = Transaction.new(
-            date: Date.parse(row[:completed_date]),
-            amount: (row[:paid_out_gbp].presence || row[:paid_in_gbp].presence).to_d,
+            date: Date.parse(row[:operation_date]),
+            amount: row[:amount].to_d,
             transaction_type: is_income ? 'income' : 'expense',
-            raw_description: "[#{row[:category]}] #{description}",
-            description: "[#{row[:category]}] #{description}",
-            original_date: Date.parse(row[:completed_date]),
-            source: 'revolut'
+            raw_description: description,
+            description: description,
+            original_date: Date.parse(row[:operation_date]),
+            source: 'sabadell'
           )
 
           save_transaction!(t) if !t.has_dupe?
