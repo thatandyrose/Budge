d68ef85aa0d31e25b2631503302f8a28e806ef6b
thatandyrose <thatandyrose@gmail.com>
2020-04-13 09:49:04 +0200

    forced ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  http_basic_authenticate_with name: "andy", password: "catch22"
+  http_basic_authenticate_with name: "andy", password: "viocorp11"
 end
